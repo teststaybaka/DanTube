ef733a1347381446f756aa33e2f5c0a8a63376f7
Tianfan Wang <tianfanw@gmail.com>
2014-12-29 17:00:22 -0500

    add models of danmaku and video

--- urls.py
+++ urls.py
@@ -10,5 +10,7 @@ config = {'webapp2_extras.sessions': session_arg}
 application = webapp2.WSGIApplication(
     [
         webapp2.Route(r'/', views.Home, name="home"),
+        webapp2.Route(r'/video', views.Video, name="video"),
+        webapp2.Route(r'/danmaku', views.Danmaku, name="danmaku"),
         ], debug=True
     , config=config)
