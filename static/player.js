0d871d088c4c4d7cf38bda26456b0727566c6bc6
teststaybaka <teststaybaka@gmail.com>
2015-01-14 21:33:22 -0500

    new navigation bar; add search icon;

--- static/player.js
+++ static/player.js
@@ -180,16 +180,13 @@ function widescreen_switch(evt) {
         player_controller.style.width = "640px";
         var danmaku_input = document.getElementById("danmaku-input");
         danmaku_input.style.width = "555px";
-        var danmaku_right_mask = document.getElementById("danmaku-right-mask");
-        danmaku_right_mask.style.left = "640px";
-        danmaku_right_mask.style.height = "360px";
         var danmaku_left_mask = document.getElementById("danmaku-left-mask");
         danmaku_left_mask.style.height = "360px";
         progress_update();
 
         var list = document.querySelectorAll("div.danmaku");
         for (var i = 0; i < list.length; ++i) {
-           list[i].style.left = "640px";
+           list[i].style.right = "384px";
         }
     } else {//off
         evt.target.className = "on";
@@ -200,16 +197,13 @@ function widescreen_switch(evt) {
         player_controller.style.width = "1024px";
         var danmaku_input = document.getElementById("danmaku-input");
         danmaku_input.style.width = "939px";
-        var danmaku_right_mask = document.getElementById("danmaku-right-mask");
-        danmaku_right_mask.style.left = "1024px";
-        danmaku_right_mask.style.height = "576px";
         var danmaku_left_mask = document.getElementById("danmaku-left-mask");
         danmaku_left_mask.style.height = "576px";
         progress_update();
 
         var list = document.querySelectorAll("div.danmaku");
         for (var i = 0; i < list.length; ++i) {
-           list[i].style.left = "1024px";
+           list[i].style.right = "0";
         }
     }
 }
