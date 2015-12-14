cbb6e13d67ff6fbb11cb5a055cfb395847eaafe7
teststaybaka <teststaybaka@gmail.com>
2015-12-14 14:04:40 -0500

    working on wide player page

--- static/js/video.js
+++ static/js/video.js
@@ -2148,7 +2148,7 @@ dt.onPlayerReady = function(event) {
 		player.playVideo();
 	}
 
-	ws = new WebSocket("ws://104.197.128.98/"+clip_id+"?session="+dt.getCookie('db_session'));
+	ws = new WebSocket("ws://104.197.128.177/"+clip_id+"?session="+dt.getCookie('db_session'));
     ws.onopen = function (event) {
         console.log('WebSocket connected!');
     }
